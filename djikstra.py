778b3e43806b61075cf8b9feea938bc485b15138
Philipp Fehre <philipp.fehre@googlemail.com>
2013-08-15 14:35:14 +0200

    added some shortest path

--- djikstra.py
+++ djikstra.py
@@ -1,4 +1,4 @@
-def djikstra(graph, source):
+def djikstra(graph, source, end=None):
     dist = {}
     previous = {}
     dist[source] = 0
@@ -8,6 +8,7 @@ def djikstra(graph, source):
         nodes = [ (x, dist[x]) for x in unsettled_nodes if dist.has_key(x) ]
         nodes.sort(lambda x,y: cmp(x[1], y[1]))
         current = nodes[0]
+        if current[0] == end: break
         unsettled_nodes.remove(current[0])
         settled_nodes.append(current[0])
         dests = [ x for x in graph[current[0]].keys() if x not in settled_nodes ]
@@ -16,10 +17,19 @@ def djikstra(graph, source):
             new_distance = dist[current[0]] + distance
             if not dist.has_key(dest_node) or dist[dest_node] > new_distance:
                 dist[dest_node] = new_distance
+                previous[dest_node] = current[0]
                 unsettled_nodes.append(dest_node)
-    return dist
+    return dist, previous
 
-
+def shortest_path(graph, start, end):
+    dist, prev = djikstra(graph, start, end)
+    path = []
+    while True:
+        path.append(end)
+        if end == start: break
+        end = prev[end]
+    path.reverse()
+    return path
 
 Graph = {'s':{'u':10, 'x':5},
          'u':{'v':1, 'x':2},
@@ -28,5 +38,9 @@ Graph = {'s':{'u':10, 'x':5},
          'y':{'s':7, 'v':6}}
 
 for node in Graph.keys():
-    print "from {0}: {1}".format(node, djikstra(Graph, node))
+    dist, _ = djikstra(Graph, node)
+    print "from {0}: {1}".format(node, dist)
+
+path = shortest_path(Graph, 's', 'v')
+print "path form {0} to {1}: {2}".format('s', 'v', path)
 
